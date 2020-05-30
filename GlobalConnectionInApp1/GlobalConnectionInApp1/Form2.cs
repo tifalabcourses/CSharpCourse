6c8a82b81f6a958b58ed5248f46da47707dbba43
tifasamir <mostafa87eltayeb@gmail.com>
2020-05-30 23:44:06 +0200

    'connstr'

---
+++ GlobalConnectionInApp1/GlobalConnectionInApp1/Form2.cs
@@ -0,0 +1,54 @@
+﻿using GlobalConnectionInApp1.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GlobalConnectionInApp1
+{
+    public partial class Form2 : Form
+    {
+        static SqlConnection con;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            con = DbHelper.getCon();
+            loadAttachTable();
+        }
+
+        public void loadAttachTable()
+        {
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Attachments2", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Attachments2");
+
+
+            dataGridView1.DataSource = ds.Tables["Attachments2"];
+     
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+        }
+    }
+}
---
+++ GlobalConnectionInApp1/GlobalConnectionInApp1/Util/DbHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace GlobalConnectionInApp1.Util
+{
+    class DbHelper
+    {
+         public static string connetionString = @"Data Source=TIFALAB\MSSQL16;Initial Catalog=School;Trusted_Connection=true;";
+         static   SqlConnection cnn   = new SqlConnection(connetionString);
+
+
+
+        
+
+               public static SqlConnection getCon()
+        {
+               return cnn;
+        }
+    }
+}
